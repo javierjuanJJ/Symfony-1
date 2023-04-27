97464e60b295f302b22bf01b9067620372480e26
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2023-04-27 11:33:29 +0200

    Commit 1

--- src/Controller/MakeController.php
+++ src/Controller/MakeController.php
@@ -4,16 +4,20 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MakeController extends AbstractController
 {
-    #[Route('/make', name: 'app_make')]
-    public function index(): JsonResponse
+//    #[Route('/make', name: 'app_make')]
+    #[Route('/', name: 'home')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MakeController.php',
-        ]);
+        return new Response(content: '<h1>Welcome freeCodeCamp!</h1>');
+    }
+    #[Route('/custom', name: 'custom')]
+    public function custom(): Response
+    {
+        return new Response(content: '<h1>Custom page!</h1>');
     }
 }
